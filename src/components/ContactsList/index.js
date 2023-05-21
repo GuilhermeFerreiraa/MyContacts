8f88ed32d677381288e3681fb301b97ee8690f9c
teste-agua-edu <gui.2001@hotmail.com>
2023-05-21 00:51:08 -0300

    component lisContainer and Card created

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Card, Container, Header, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/delete.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +13,76 @@ export default function ContactsList() {
         <strong>3 Contatos</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Guilherme Ferreira</strong>
+              <small>Instagram</small>
+            </div>
+            <span>email@email.com</span>
+            <span>(11)99114-2375</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Guilherme Ferreira</strong>
+              <small>Instagram</small>
+            </div>
+            <span>email@email.com</span>
+            <span>(11)99114-2375</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Guilherme Ferreira</strong>
+              <small>Instagram</small>
+            </div>
+            <span>email@email.com</span>
+            <span>(11)99114-2375</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+
+      </ListContainer>
     </Container>
   );
 }
--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -24,9 +24,82 @@ export const Header = styled.header`
   font-weight: bold;
   transition: all 0.2s ease-in;
 
-  &:hover{
+  &:hover {
    background-color: ${({ theme }) => theme.colors.primary.main};
    color: #fff;
+  }
+ }
+`;
+
+export const ListContainer = styled.div`
+ margin-top: 24px;
+
+ header {
+  margin-bottom: 8px;
+
+  button {
+   column-gap: 8px;
+   background: transparent;
+   border: none;
+   display: flex;
+   align-items: center;
+
+   span {
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.primary.main};
+   }
+  }
+ }
+`;
+
+export const Card = styled.div`
+ background-color: #fff;
+ box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+ border-radius: 4px;
+ padding: 16px;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+
+ & + & {
+  margin-top: 16px;
+ }
+
+ .info {
+  .contact-name {
+   display: flex;
+   column-gap: 8px;
+   align-items: center;
+
+   small {
+    background: ${({ theme }) => theme.colors.primary.lighter};
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-transform: uppercase;
+    font-weight: bold;
+    border-radius: 4px;
+    padding: 4px;
+   }
+  }
+
+  span {
+   display: block;
+   font-size: 14px;
+   color: ${({ theme }) => theme.colors.gray[200]};
+  }
+ }
+
+ .actions {
+  display: flex;
+  column-gap: 8px;
+  align-items: center;
+
+  button {
+   background: transparent;
+   border: none;
+  }
+
+  a {
+
   }
  }
 `;
