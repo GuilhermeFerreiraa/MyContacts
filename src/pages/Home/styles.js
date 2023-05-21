bc2392e188996594ca8c2f90968f89493a09acab
teste-agua-edu <gui.2001@hotmail.com>
2023-05-21 01:15:31 -0300

    refactoring code from home and header

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,11 +4,32 @@ export const Container = styled.div`
  margin-top: 32px;
 `;
 
+export const InputSearchContainer = styled.div`
+ width: 100%;
+
+ input {
+  width: 100%;
+  border: none;
+  border-radius: 25px;
+  background-color: "#fff";
+  height: 50px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 15px 16px;
+  font-size: 16px;
+  color: #BCBCBC;
+  outline: 0;
+
+  &::placeholder {
+   color: #BCBCBC;
+  }
+ }
+`;
+
 export const Header = styled.header`
  display: flex;
  align-items: center;
  justify-content: space-between;
-
+ margin-top: 32px;
  strong {
   color: #222222;
   font-size: 24px;
