aea7924978a6324a1c7410e04222146271698fe4
teste-agua-edu <gui.2001@hotmail.com>
2023-05-22 22:14:46 -0300

    status error on input component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,13 +10,13 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/delete.svg';
-import Loader from '../../components/Loader';
+// import Loader from '../../components/Loader';
 // import Modal from '../../components/Modal';
 
 export default function Home() {
   return (
     <Container>
-      <Loader />
+      {/* <Loader /> */}
       {/* <Modal
         danger
         description="Esta ação não poderá ser desfeita!"
