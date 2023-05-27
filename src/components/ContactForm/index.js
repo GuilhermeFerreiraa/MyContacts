670433cf3cc8507d6f2ba0796ac7f67c8bbc59ba
teste-agua-edu <gui.2001@hotmail.com>
2023-05-26 23:13:11 -0300

    validate components inserted

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,28 +1,94 @@
 import PropTypes from 'prop-types';
-import { Form, ButtonContainer } from './styles';
+import { useState } from 'react';
+import Button from '../Button';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
-import Button from '../Button';
+import { ButtonContainer, Form } from './styles';
+import isEmailValid from '../../utils/isEmailValid';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  // One way data binding - react = single source of truth
+  // Two way data binding - angular e vue
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatório' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (errorAlreadyExists) {
+        return;
+      }
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'E-mail Inválido' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'));
+    }
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
+  console.log(errors);
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+  // eslint-disable-next-line react/jsx-no-bind
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input error={getErrorMessageByFieldName('name')} placeholder="Nome" value={name} onChange={handleNameChange} />
       </FormGroup>
 
-      <FormGroup error="O formato do e-mail é inválido.">
-        <Input error placeholder="E-mail" />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder="Telefone" />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input error={getErrorMessageByFieldName('email')} placeholder="E-mail" value={email} onChange={handleEmailChange} />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="Discord">Discord</option>
         </Select>
       </FormGroup>
 
@@ -34,5 +100,9 @@ export default function ContactForm({ buttonLabel }) {
 }
 
 ContactForm.propTypes = {
-  buttonLabel: PropTypes.string.isRequired,
+  buttonLabel: PropTypes.string,
+};
+
+ContactForm.defaultProps = {
+  buttonLabel: PropTypes.string,
 };
--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -3,12 +3,11 @@ import { styled } from 'styled-components';
 export const Container = styled.div`
  & + & {
   margin-top: 16px;
-
-  small {
-   display: block;
-   font-size: 12px;
-   margin-top: 8px;
-   color: ${({ theme }) => theme.colors.danger.main};
-  }
+ }
+ small {
+  display: block;
+  font-size: 12px;
+  margin-top: 8px;
+  color: ${({ theme }) => theme.colors.danger.main} !important;
  }
 `;
