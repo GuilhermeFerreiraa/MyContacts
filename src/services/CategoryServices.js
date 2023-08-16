8485b0feeb2604d7788e744e32822f913c5bd93c
teste-agua-edu <gui.2001@hotmail.com>
2023-08-15 22:58:04 -0300

    mappers added

--- src/services/CategoryServices.js
+++ src/services/CategoryServices.js
@@ -1,3 +1,4 @@
+import CategoryMapper from './mappers/CategoryMapper';
 import HttpClient from './utils/HttpClient';
 
 /* eslint-disable class-methods-use-this */
@@ -6,8 +7,9 @@ class CategoriesService {
     this.httpClient = new HttpClient('http://localhost:3001');
   }
 
-  listCategories() {
-    return this.httpClient.get('/categories');
+  async listCategories() {
+    const categories = this.httpClient.get('/categories');
+    return categories.map(CategoryMapper.toDomain);
   }
 }
 
